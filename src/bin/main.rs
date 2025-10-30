e3d512cc1cc839bfe259e6b18803a379f1f0e77c
David Watson <david@davidwatson.io>
2025-10-30 18:41:58 +0000

    user mode and trap handling

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,16 +1,9 @@
 #![no_std]
 #![no_main]
-#![deny(
-    clippy::mem_forget,
-    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
-    holding buffers for the duration of a data transfer."
-)]
 
-use core::fmt::Write;
+use core::arch::asm;
 use esp_hal::clock::CpuClock;
 use esp_hal::main;
-use esp_hal::time::{Duration, Instant};
-use esp_hal::{peripherals::Peripherals, uart::Uart};
 
 esp_bootloader_esp_idf::esp_app_desc!();
 
@@ -19,17 +12,44 @@ fn panic(_: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
+fn uart_write_word(word: usize) {
+    let uart_address = 0x6000_0000_usize;
+    unsafe {
+        asm!("sw {1}, 0({0})", in(reg) uart_address, in(reg) word);
+    }
+}
 
 #[main]
 unsafe fn main() -> ! {
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let _peripherals = esp_hal::init(config);
-    let peripherals = Peripherals::steal();
-    let mut uart0 = Uart::new(peripherals.UART0, esp_hal::uart::Config::default()).unwrap();
 
+    let trap_handle_addr: usize = trap_handle as usize;
+    asm!("csrw mepc, {0}", in(reg) trap_handle_addr);
+    asm!("mret");
+    uart_write_word(69);
+    uart_write_word(76);
+    uart_write_word(76);
+    uart_write_word(79);
+
+    let test = 1234;
+    let test_ptr: *const i32 = &test;
+    let mut test_addr: usize = test_ptr as usize;
     loop {
-        writeln!(uart0, "hello!");
-        let delay_start = Instant::now();
-        while delay_start.elapsed() < Duration::from_millis(500) {}
+        uart_write_word(test_addr % 10 + 48);
+        test_addr /= 10;
+        if test_addr == 0 { break; }
     }
+
+    loop {}
+}
+
+unsafe fn trap_handle() -> ! {
+    uart_write_word(80);
+    uart_write_word(80);
+    uart_write_word(80);
+    uart_write_word(80);
+    uart_write_word(80);
+    uart_write_word(80);
+    loop {}
 }
